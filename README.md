8fdbaf01d62de5257ed84345a895a4f67d1b5da9
owhefave <owhefave@gmail.com>
2023-11-15 06:43:39 -0800

    beautiful code

---
+++ README.md
@@ -0,0 +1,5 @@
+# SIMPLE SHELL PROJECT
+
+Project Team
+1. Owhe Efe Favour
+1. Ojeyemi Olajide
---
+++ beautiful.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+* main - greetings to my partner
+* Return: Always zero
+*/
+
+int main(void)
+{
+	printf("Hello, Jide, Lets do this together\n");
+	return (0);
+}
