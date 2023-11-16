69bea0b89bd24d97dded799dc86f9075f54b0b1a
samson3reeps <samson3reeps@gmail.com>
2023-11-16 05:13:43 -0800

    beautiful

--- beautiful.c
+++ beautiful.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of the program
  *
- * Return: 0 on success, otherwise an error code
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char *message = "Hello, Betty!";
+	char message[] = "Hello, Jide, Lets do this together\n";
 
-	printf("%s\n", message);
+	write(STDOUT_FILENO, message, sizeof(message) - 1);
 	return (0);
 }
 
