24bb8c3ddecb09e4adb2978126624b3cfa38ac3a
owhefave <owhefave@gmail.com>
2023-11-16 02:11:17 -0800

    task 0

--- shell.c
+++ shell.c
@@ -3,68 +3,85 @@
 #define MAX_COMMAND_LENGTH 128
 
 /* Display the shell prompt */
-void display_prompt() {
+void display_prompt()
+{
     printf("#cisfun$ ");
     fflush(stdout);
 }
 
 /* Execute the specified command */
-void execute_command(char *command) {
+void execute_command(char *command)
+{
     pid_t pid;
-
+    
     pid = fork();
-
-    if (pid == -1) {
+    
+    if (pid == -1)
+    {
         /* Handle fork error */
         perror("fork");
         exit(EXIT_FAILURE);
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         /* Child process */
-        if (execlp(command, command, (char *)NULL) == -1) {
+        if (execlp(command, command, (char *)NULL) == -1)
+        {
             /* Handle the case when the executable is not found */
             perror("exec");
             exit(EXIT_FAILURE);
         }
-    } else {
+    }
+    else
+    {
         /* Parent process */
         int status;
         waitpid(pid, &status, 0);
     }
 }
 
-int main() {
+int main()
+{
     char command[MAX_COMMAND_LENGTH];
-
-    while (1) {
+    
+    while (1)
+    {
         size_t cmd_len;
-
+        
         display_prompt();
-
+        
         /* Read a command from the user */
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            if (feof(stdin)) {
+        
+        if (fgets(command, sizeof(command), stdin) == NULL)
+        {
+            if (feof(stdin))
+            {
                 /* Exit the shell on EOF (Ctrl+D) */
                 printf("\nExiting the shell. Goodbye!\n");
                 break;
-            } else {
+            }
+            else
+            {
                 /* Handle fgets error */
                 perror("fgets");
                 exit(EXIT_FAILURE);
             }
         }
-
         /* Remove the trailing newline character */
         cmd_len = strlen(command);
-        if (cmd_len > 0 && command[cmd_len - 1] == '\n') {
+        if (cmd_len > 0 && command[cmd_len - 1] == '\n')
+        {
             command[cmd_len - 1] = '\0';
         }
-
+        
         /* Execute the command */
-        if (strlen(command) > 0) {
+        if (strlen(command) > 0)
+        {
             execute_command(command);
         }
     }
 
-    return 0;
+    return (0);
 }
+
 
