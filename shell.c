ca1ff28275dae5a31851f846f7321de78be7b29b
samson3reeps <samson3reeps@gmail.com>
2023-11-17 18:41:43 -0800

    fix_3

--- shell.c
+++ shell.c
@@ -8,48 +8,63 @@
 #define MAX_COMMAND_LENGTH 128
 
 /* Display the shell prompt */
-void display_prompt(void) {
-	printf("#cisfun$ ");
+void display_prompt(void)
+{
+	printf("#cisfun$");
 	fflush(stdout);
 }
 
 /* Execute the specified command */
-void execute_command(char *command) {
+void execute_command(char *command)
+{
 	pid_t pid;
 	int status;
 
 	pid = fork();
 
-	if (pid == -1) {
+	if (pid == -1)
+	{
 		/* Handle fork error */
 		perror("fork");
 		exit(EXIT_FAILURE);
-	} else if (pid == 0) {
+	}
+	else if (pid == 0)
+	{
 		/* Child process */
-		if (execlp(command, command, (char *)NULL) == -1) {
+		if (execlp(command, command, (char *)NULL) == -1)
+		{
 			/* Handle the case when exec not found */
 			fprintf(stderr, "Error: Cannot execute '%s'. Command not found.\n", command);
 			exit(EXIT_FAILURE);
 		}
-	} else {
+	}
+	else
+	{
 		/* Parent process */
 		waitpid(pid, &status, 0);
 	}
 }
 
-int main(void) {
+int main(void)
+{
 	char command[MAX_COMMAND_LENGTH];
+	size_t cmd_len;
 
-	while (1) {
+	while (1)
+	{
 		display_prompt();
 
 		/* Read a command from the user */
-		if (fgets(command, sizeof(command), stdin) == NULL) {
-			if (feof(stdin)) {
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			if (feof(stdin))
+			{
 				/* Exit the shell on EOF (Ctrl+D) */
 				printf("\nExiting the shell. Goodbye!\n");
 				break;
-			} else {
+			}
+			else
+			{
 				/* Handle fgets error */
 				perror("fgets");
 				exit(EXIT_FAILURE);
@@ -57,13 +72,15 @@ int main(void) {
 		}
 
 		/* Remove the trailing newline character */
-		size_t cmd_len = strlen(command);
-		if (cmd_len > 0 && command[cmd_len - 1] == '\n') {
+		cmd_len = strlen(command);
+		if (cmd_len > 0 && command[cmd_len - 1] == '\n')
+		{
 			command[cmd_len - 1] = '\0';
 		}
 
 		/* Execute the command */
-		if (strlen(command) > 0) {
+		if (strlen(command) > 0)
+		{
 			execute_command(command);
 		}
 	}
